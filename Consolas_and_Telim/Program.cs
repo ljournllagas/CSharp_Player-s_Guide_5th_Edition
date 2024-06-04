3055cf6560a9487b985c205c1ca4107dc68d66b8
Ljourn Llagas <llagas.ljournkevin@bdo.com.ph>
2024-06-04 22:29:34 +0800

    Level 3 - Consolas and Telim

---
+++ Consolas_and_Telim/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is the bread for?");
+var name = Console.ReadLine();
+Console.WriteLine("Noted. " + name + " got bread.");
+Console.ReadKey();
