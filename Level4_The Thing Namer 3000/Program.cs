824cb336080a4230d2a57131e29973f639245850
Ljourn Llagas <llagas.ljournkevin@bdo.com.ph>
2024-06-04 22:42:09 +0800

    Level 4 - The Thing Namer 3000

---
+++ Level4_The Thing Namer 3000/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+string thing = Console.ReadLine(); //stores the thing that you're talking about
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string describe = Console.ReadLine(); /* 
+                                * describe what it is
+                                */
+string doom = "Doom"; 
+string three_thousand = "3000";
+Console.WriteLine("The " + describe + " " + thing + " of " + doom + " " + three_thousand + "!");
+Console.ReadLine();
